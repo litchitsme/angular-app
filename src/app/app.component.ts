1ecb3d3caab00536402bc43350a082d7c5d04917
Karel Segers <karelsegers@outlook.com>
2024-12-11 08:54:02 +0100

    Added a 'user' child component and played around with the possibilities.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // Class component. Zit alle component logica in
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
-@Component({
+@Component({ // Decorator. Geeft extra data mee aan class.
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-app';
+  name = "Karel";
+  xmas = false;
+  operatingSystems = [
+    { id: 1, name: 'Windows' },
+    { id: 2, name: 'Linux' },
+    { id: 3, name: 'Mac' }]
 }
